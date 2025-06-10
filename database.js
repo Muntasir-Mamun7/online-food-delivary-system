425cfc4d3e73f8aa127d9c3b59693a1a7595d7b1
Muntasir Al Mamun <138435835+Muntasir-Mamun7@users.noreply.github.com>
2025-06-10 08:17:37 +0800

    Update database.js

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ const sqlite3 = require('sqlite3').verbose();
 const bcrypt = require('bcrypt');
 const path = require('path');
 
-// Create a new database connection
+// Create a new database connection s
 const db = new sqlite3.Database(path.join(__dirname, 'food_delivery.db'));
 
 // Initialize database
